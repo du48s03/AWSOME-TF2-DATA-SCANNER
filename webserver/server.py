863a445dce8c194d9ae38cffe93c3bc68a131325
ewu <ewu@dyn-160-39-131-46.dyn.columbia.edu>
2015-10-15 22:27:46 -0400

    updated code

--- webserver/server.py
+++ webserver/server.py
@@ -4,11 +4,15 @@
 Columbia W4111 Intro to databases
 Example webserver
 
-To run
+To run locally
 
     python server.py
 
 Go to http://localhost:8111 in your browser
+
+
+A debugger such as "pdb" may be helpful for debugging.
+Read about it online.
 
 eugene wu 2015
 """
@@ -23,32 +27,59 @@ app = Flask(__name__, template_folder=tmpl_dir)
 
 
 #
-# XXX: set this to your database
+# The following uses the sqlite3 database test.db -- you can use this for debugging purposes
+# However for the project you will need to connect to your Part 2 database in order to use the
+# data
+#
+# XXX: The URI should be in the format of: 
+#
+#     postgresql://USER:PASSWORD@w4111db1.cloudapp.net:5432/proj1part2
+#
+# For example, if you had username ewu2493, password foobar, then the following line would be:
+#
+#     DATABASEURI = "postgresql://ewu2493:foobar@w4111db1.cloudapp.net:5432/proj1part2"
 #
 DATABASEURI = "sqlite:///test.db"
 
 
 #
-# after these statements, you should see a file test.db in your webserver/ directory
-# this is a sqlite database that you can query like psql typing in the command line:
+# This line creates a database engine that knows how to connect to the URI above
+#
+engine = create_engine(DATABASEURI)
+
+
+#
+# START SQLITE SETUP CODE
+#
+# after these statements run, you should see a file test.db in your webserver/ directory
+# this is a sqlite database that you can query like psql typing in the shell command line:
 # 
 #     sqlite3 test.db
 #
 # The following sqlite3 commands may be useful:
 # 
-#     .tables               will list the tables in the database
-#     .schema <tablename>   print CREATE TABLE statement for table
+#     .tables               -- will list the tables in the database
+#     .schema <tablename>   -- print CREATE TABLE statement for table
+# 
+# The setup code should be deleted once you switch to using the Part 2 postgresql database
 #
-engine = create_engine(DATABASEURI)
+engine.execute("""DROP TABLE IF EXISTS test;""")
 engine.execute("""CREATE TABLE IF NOT EXISTS test (
   id serial,
   name text
-)""")
+);""")
+engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+#
+# END SQLITE SETUP CODE
+#
+
+
 
 @app.before_request
 def before_request():
   """
-  This function is run at the beginning of every web request
+  This function is run at the beginning of every web request 
+  (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request
 
   The variable g is globally accessible
@@ -56,19 +87,35 @@ def before_request():
   try:
     g.conn = engine.connect()
   except:
+    print "uh oh, problem connecting to database"
     import traceback; traceback.print_exc()
     g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
   """
-  At the end of the web request, this makes sure to close the database connection
+  At the end of the web request, this makes sure to close the database connection.
+  If you don't the database could run out of memory!
   """
   try:
     g.conn.close()
   except Exception as e:
     pass
 
+
+#
+# @app.route is a decorator around index() that means:
+#   run index() whenever the user tries to access the "/" path using a POST or GET request
+#
+# If you wanted the user to go to e.g., localhost:8111/foobar/ with POST or GET then you could use
+#
+#       @app.route("/foobar/", methods=["POST", "GET"])
+#
+# PROTIP: (the trailing / in the path is important)
+# 
+# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
+# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
+# 
 @app.route('/', methods=["POST", "GET"])
 def index():
   """
@@ -80,7 +127,19 @@ def index():
 
   See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
   """
+
+  # DEBUG: this is debugging code to see what request looks like
   print request.args
+
+
+  #
+  # example of a database query
+  #
+  cursor = g.conn.execute("SELECT name FROM test")
+  names = []
+  for result in cursor:
+    names.append(result['name'])  # can also be accessed using result[0]
+  cursor.close()
 
   #
   # Flask uses Jinja templates, which is an extension to HTML where you can
@@ -88,27 +147,46 @@ def index():
   # (you can think of it as simple PHP)
   # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
   #
-  # context are the variables that are passed to the index.html template.
-  # for example, "data" below will be accessible as a variable in index.html:
+  # You can see an example template in templates/index.html
   #
-  #     # will print str(data)
+  # context are the variables that are passed to the template.
+  # for example, "data" key in the context variable defined below will be 
+  # accessible as a variable in index.html:
+  #
+  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
   #     <div>{{data}}</div>
   #     
   #     # creates a <div> tag for each element in data
+  #     # will print: 
+  #     #
+  #     #   <div>grace hopper</div>
+  #     #   <div>alan turing</div>
+  #     #   <div>ada lovelace</div>
+  #     #
   #     {% for n in data %}
   #     <div>{{n}}</div>
   #     {% endfor %}
   #
-  context = dict( data = [1, 2] )
+  context = dict( data = names )
 
 
   #
   # render_template looks in the templates/ folder for files.
-  # for example, the below file is template/index.html
+  # for example, the below file reads template/index.html
   #
   return render_template("index.html", **context)
 
-
+#
+# This is an example of a different path.  You can see it at
+# 
+#     localhost:8111/another/
+#
+# notice that the functio name is another() rather than index()
+# the functions for each app.route needs to have different names
+#
+@app.route('/another/', methods=["POST", "GET"])
+def another():
+  return render_template("anotherfile.html")
 
 if __name__ == "__main__":
   import click
