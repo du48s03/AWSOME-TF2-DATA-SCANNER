e1772501d4374e4f446b8c1dc6626b3c85610020
Ubuntu <azureuser@ading.ading.c1.internal.cloudapp.net>
2015-11-08 22:37:07 +0000

    editted the server.py

--- webserver/server.py
+++ webserver/server.py
@@ -118,9 +118,15 @@ def teardown_request(exception):
 # 
 @app.route("/general_query/", methods=["POST", "GET"])
 def myappfunction():
+  #keys(request.args) = ['attr', 'attr_val', 'entity', 'results']
+  print "hello"
+  results = list() # get the former results, 
+  print request.args
+  print request.args['attr']
+  print request.args['attr_val']
+  print request.args['entity']
 
   return render_template("general_query.html")
-
 
 
 
@@ -137,52 +143,7 @@ def index():
   See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
   """
 
-  # DEBUG: this is debugging code to see what request looks like
-  #print request.args
-
-
-  #
-  # example of a database query
-  #
-  #cursor = g.conn.execute("SELECT name FROM team")
-  #names = []
-  #for result in cursor:
-  #  names.append(result['name'])  # can also be accessed using result[0]
-  #cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  #context = dict( data = names )
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
+  
   return render_template("index.html")
 
 #
