ddece204e2b772723c2426ca01eb4b486280851a
Ubuntu <azureuser@ading.ading.c1.internal.cloudapp.net>
2015-11-11 05:38:59 +0000

    added the utils.py

---
+++ webserver/utils.py
@@ -0,0 +1,14 @@
+import re
+
+def sanitize(string):
+  """sanitize the input string. Escaping anyspecial characters except for '!', '&', ':', '_', '^', and '-'
+     returns the sanitized string. 
+  """
+  ret = ""
+  for c in string:
+    if(re.match(r'[^a-zA-Z0-9\!\&\:\_\^\-]', ""+c)):
+      ret+='\\'
+      ret+=c
+    else:
+      ret+=c
+  return ret
